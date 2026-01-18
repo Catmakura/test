6ef782f702fd3ecfa44b1b117db9b7cb3eb9bc17
Catmakura <myhomemailaddress.01@gmail.com>
2026-01-18 10:37:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="description" content="test">
+    <meta name="aaaa" content="aaa">
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+    <header>
+      <div class="script">
+        <script src="script.js">
+          function showTime()
+        </script>
+      </div>
+    </header>
+  </body>
+</html>
